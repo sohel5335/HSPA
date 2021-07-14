43c9b500c90c174a124f906c0a35d169c78da830
sohel5335 <rsohel5335@gmail.com>
2021-07-15 02:01:29 +0600

    create webapi

---
+++ WebAPI/Controllers/CityController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        public CityController()
+        {
+        }
+
+        [HttpGet("")]
+        public IEnumerable<string> GetTModels()
+        {
+            return new string[] { "Dhaka","New York","Canda"};
+        }
+
+      
+
+    }
+}
